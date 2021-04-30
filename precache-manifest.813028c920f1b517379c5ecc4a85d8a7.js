e8051a1b6390459a3a96683d5842ca7406da146b
kwmin90 <kwmin90@users.noreply.github.com>
2021-04-30 03:58:07 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.813028c920f1b517379c5ecc4a85d8a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd998d9270fd554ac8fc9b9c7f5b859",
+    "url": "/game_list_app_igdb_react/index.html"
+  },
+  {
+    "revision": "8f8a3adafce129f44a44",
+    "url": "/game_list_app_igdb_react/static/css/main.45edfdb2.chunk.css"
+  },
+  {
+    "revision": "37379a95e6e2a4d411ac",
+    "url": "/game_list_app_igdb_react/static/js/2.1afd0f2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game_list_app_igdb_react/static/js/2.1afd0f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8a3adafce129f44a44",
+    "url": "/game_list_app_igdb_react/static/js/main.22f0ef06.chunk.js"
+  },
+  {
+    "revision": "83c5f7cd72977887e9cf",
+    "url": "/game_list_app_igdb_react/static/js/runtime-main.24f6fd7c.js"
+  }
+]);
